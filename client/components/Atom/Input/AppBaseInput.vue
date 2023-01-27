72f2add26ebea2d98e933d629b27577e6cb25366
hikari-8 <kanbarusuruga0127@gmail.com>
2023-01-27 14:28:52 +0900

    darkモードを消去しました

--- client/components/Atom/Input/AppBaseInput.vue
+++ client/components/Atom/Input/AppBaseInput.vue
@@ -3,7 +3,7 @@
         <input
             v-model="localValue"
             :type="type"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         />
     </div>
 </template>
